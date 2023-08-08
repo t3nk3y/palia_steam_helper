9b3ed047070a2f0329ee301be887b41e9b3cd87e
T3NK3Y <mattie@t3nk3y.com>
2023-08-08 18:19:58 -0400

    Update init and readme for github repo

--- install_palia.sh
+++ install_palia.sh
@@ -4,8 +4,7 @@
 # Purpose:   Launch either the PaliaLauncher for install, or the installed PaliaLauncher
 # Arguments: None for now
 
-#curl https://www.opscode.com/chef/install.sh | bash
-curl -O https://github/palia_steam_helper.sh
+curl -L -O https://raw.githubusercontent.com/t3nk3y/palia_steam_helper/main/palia_steam_helper.sh
 chmod +x palia_steam_helper.sh
 
 encodedUrl="steam://addnonsteamgame/$(python3 -c "import urllib.parse;print(urllib.parse.quote(\"$PWD/palia_steam_helper.sh\", safe=''))")"
