d74aa701dbcb5e70625559d7374005fa9d60e521
T3NK3Y <mattie@t3nk3y.com>
2023-08-13 20:14:18 -0400

    More friendly GetPaliaUpdates.sh

--- GetPaliaUpdates.sh
+++ GetPaliaUpdates.sh
@@ -1,42 +1,61 @@
 #!/usr/bin/env bash
 
-# Author:    Give yourself some credit
-# Purpose:   What this script does
-# Arguments: What arguments can be passed to the script
+# Author:    Madalee
+# Purpose:   This will grab/extract the Palia game files and updates
+#            It's intended to run on Mac OS without any additional
+#            dependencies, but may work on other Linux environments.
 
 INSTALLATION_DIRECTORY=$(pwd)
 
 function download_file() {
-  FILE_URL=$1
+  FILE_URL="$1"
 
   FILENAME=$(basename -- "$FILE_URL")
   EXT="${FILENAME##*.}"
 
-  if [[ $EXT == "exe" ]]
-  then
-    DESTINATION_PATH="$INSTALLATION_DIRECTORY/Palia/Binaries/Win64/$FILENAME"
+  if [[ $EXT == "zip" ]]; then
+    DESTINATION_PATH="$INSTALLATION_DIRECTORY/Client"
+    if [[ ! -f "$DESTINATION_PATH/Build.version" ]]; then
+      echo "Build.version is missing, so we are going to download the base zip now..."
+      echo "This will take a long time, but you can restart if things get interrupted."
+      echo ""
+      echo ""
+      curl -L -C - -o "$FILENAME" "$FILE_URL"
+      echo ""
+      echo ""
+    fi
 
-    curl -L --create-dirs -C - --progress-bar -o $DESTINATION_PATH $FILE_URL
-  elif [[ $EXT == "pak" ]]
-  then
-    DESTINATION_PATH="$INSTALLATION_DIRECTORY/Palia/Content/Paks/$FILENAME"
+    if [[ -f "$FILENAME" ]]; then
+      echo "Unzipping base Palia game files, this will take a long time, standby..."
+      unzip -o -d "$DESTINATION_PATH" -u "$FILENAME"
+      echo "Done unzipping base game files..."
+      echo ""
+      echo ""
+    fi
+  elif [[ $EXT == "exe" ]]; then
+    echo "Checking/downloading $FILENAME..."
+    DESTINATION_PATH="$INSTALLATION_DIRECTORY/Client/Palia/Binaries/Win64/$FILENAME"
 
-    #if [ ! -f "$DESTINATION_PATH" ]
-    #then
-      curl -L --create-dirs -C - --progress-bar -o $DESTINATION_PATH $FILE_URL
+    curl -L --create-dirs -C - --progress-bar -o "$DESTINATION_PATH" "$FILE_URL"
+  elif [[ $EXT == "pak" ]]; then
+    echo "Checking/downloading $FILENAME..."
+    DESTINATION_PATH="$INSTALLATION_DIRECTORY/Client/Palia/Content/Paks/$FILENAME"
+
+    #if [[ ! -f "$DESTINATION_PATH" ]]; #then
+      curl -L --create-dirs -C - --progress-bar -o "$DESTINATION_PATH" "$FILE_URL"
     #fi
   fi
 }
 
-regex="shadercache/([^/]+)/"
-[[ $STEAM_COMPAT_MEDIA_PATH =~ $regex ]]
-export SteamAppId=${BASH_REMATCH[1]}
-
-
-
+echo Grabbing and processing manifest...
+echo ""
+echo ""
 regex="\"URL\": \"([^\"]+)\""
 curl -L -s https://update.palia.com/manifest/PatchManifest.json | grep https |
 while read in; do
     [[ $in =~ $regex ]]
     download_file ${BASH_REMATCH[1]}
 done
+echo ""
+echo ""
+echo "All done..."
