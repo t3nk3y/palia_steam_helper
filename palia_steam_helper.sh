e257358ae8d2e75bf4a409b0e6f181684e5235d2
T3NK3Y <mattie@t3nk3y.com>
2023-08-25 22:02:56 -0400

    -Changed launcher aquisition method

--- palia_steam_helper.sh
+++ palia_steam_helper.sh
@@ -12,6 +12,7 @@ import sys
 import json
 import re
 import logging
+import hashlib
 
 if os.path.exists("palia_steam_helper.log"):
   os.remove("palia_steam_helper.log")
@@ -37,46 +38,103 @@ def download_progress(url, target_path, title, text):
     zenity.terminate()
     return 0
 
-def extract(zip_path, target_path, title, text):
+def extract(zip_path, target_path, title, text, filename=None):
     zenity = Popen(['zenity', '--progress', '--percentage=0','--time-remaining', f'--title={title}', f'--text={text}', '--width=400', '--height=300'], text=True, stdin=PIPE, stdout=PIPE)
     block_size = 8192
     z = zipfile.ZipFile(zip_path)
     total_size = sum((file.file_size for file in z.infolist()))
     offset = 0
     prog = 0
-    for entry_name in z.namelist():
-        if zenity.poll() != None:
-            return zenity.returncode
-        entry_info = z.getinfo(entry_name)
-        i = z.open(entry_name)
-        if entry_name[-1] != '/':
-            dir_name = os.path.dirname(entry_name)
-            p = Path(f"{target_path}/{dir_name}")
-            p.mkdir(parents=True, exist_ok=True)
-            o = open(f"{target_path}/{entry_name}", 'wb')
-            while True:
-                if zenity.poll() != None:
-                    i.close()
-                    return zenity.returncode
-                b = i.read(block_size)
-                offset += len(b)
-                prog = round(float(offset)/float(total_size) * 100,1)
-                zenity.stdin.write(f"# {prog}% complete: {text}\n")
-                zenity.stdin.write(f"{prog}\n")
-                zenity.stdin.flush()
-                if b == b'':
-                    break
-                o.write(b)
-            o.close()
+    if filename == None:
+        for entry_name in z.namelist():
+            if zenity.poll() != None:
+                return zenity.returncode
+            entry_info = z.getinfo(entry_name)
+            i = z.open(entry_name)
+            if entry_name[-1] != '/':
+                dir_name = os.path.dirname(entry_name)
+                p = Path(f"{target_path}/{dir_name}")
+                p.mkdir(parents=True, exist_ok=True)
+                o = open(f"{target_path}/{entry_name}", 'wb')
+                while True:
+                    if zenity.poll() != None:
+                        i.close()
+                        return zenity.returncode
+                    b = i.read(block_size)
+                    offset += len(b)
+                    prog = round(float(offset)/float(total_size) * 100,1)
+                    zenity.stdin.write(f"# {prog}% complete: {text}\n")
+                    zenity.stdin.write(f"{prog}\n")
+                    zenity.stdin.flush()
+                    if b == b'':
+                        break
+                    o.write(b)
+                o.close()
+            i.close()
+        z.close()
+    else:
+        i = z.open(filename)
+        dir_name = os.path.dirname(filename)
+        p = Path(f"{target_path}/{dir_name}")
+        p.mkdir(parents=True, exist_ok=True)
+        o = open(f"{target_path}/{filename}", 'wb')
+        while True:
+            if zenity.poll() != None:
+                i.close()
+                return zenity.returncode
+            b = i.read(block_size)
+            offset += len(b)
+            prog = round(float(offset)/float(total_size) * 100,1)
+            zenity.stdin.write(f"# {prog}% complete: {text}\n")
+            zenity.stdin.write(f"{prog}\n")
+            zenity.stdin.flush()
+            if b == b'':
+                break
+            o.write(b)
+        o.close()
         i.close()
-    z.close()
+        z.close()
     zenity.terminate()
     return 0
 
+def save_known_hashes():
+    with open(KNOWN_HASH_FILE,"w") as khf:
+        khf.write(json.dumps(KNOWN_HASHES))
+
+def load_known_hashes():
+    if os.path.isfile(KNOWN_HASH_FILE):
+        with open(KNOWN_HASH_FILE,"r") as khf:
+            KNOWN_HASHES.clear()
+            KNOWN_HASHES.update(json.load(khf))
+
+def file_hash(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+def validate_launcher():
+    load_known_hashes()
+    download_progress(PALIA_LAUNCHER_MANIFEST_URL, PALIA_LAUNCHER_MANIFEST, "Validate Palia Launcher", "Checking Launcher Version")
+    manifest_hash_new = file_hash(PALIA_LAUNCHER_MANIFEST)
+    if KNOWN_HASHES.get(PALIA_LAUNCHER_MANIFEST, "") != manifest_hash_new or ( os.path.isfile(PALIA_LAUNCHER) and KNOWN_HASHES.get(PALIA_LAUNCHER_EXE, "") != file_hash(PALIA_LAUNCHER) ):
+        with open(PALIA_LAUNCHER_MANIFEST, "r") as mf:
+            mani = json.load(mf)
+        launcherurl = mani['url']
+        download_progress(launcherurl, PALIA_LAUNCHER_ZIP, "Validate Palia Launcher", "Downloading New Launcher Version")
+        extract(PALIA_LAUNCHER_ZIP, PALIA_LAUNCHER_PATH, "Validate Palia Launcher", "Installing New Launcher Version", PALIA_LAUNCHER_EXE)
+        os.remove(PALIA_LAUNCHER_ZIP)
+        KNOWN_HASHES[PALIA_LAUNCHER_EXE] = file_hash(PALIA_LAUNCHER)
+        KNOWN_HASHES[PALIA_LAUNCHER_MANIFEST] = manifest_hash_new
+        save_known_hashes()
+    os.remove(PALIA_LAUNCHER_MANIFEST)
+
 def launch_palia():
+    validate_launcher()
     with Popen(['proton', 'run', f'{PALIA_LAUNCHER}'], text=True, stdout=PIPE, stderr=STDOUT) as palia_proc:
         logging.debug(palia_proc.stdout.read())
-    run(['proton', 'runinprefix', f'reg query HKEY_CLASSES_ROOT\\Installer\\Products\\A12B171E85ADD2347AB41DB302B44A77'])
+    run(['proton', 'run', f'reg query HKEY_CLASSES_ROOT\\Installer\\Products\\A12B171E85ADD2347AB41DB302B44A77'])
 
 f = re.findall(r'compatdata/([^/]+)/', os.environ['WINEPREFIX'])
 if len(f) > 0:
@@ -86,12 +144,21 @@ os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 os.environ['STEAM_COMPAT_DATA_PATH'] = "%s/steam" % os.getcwd()
 os.environ['STEAM_COMPAT_CLIENT_INSTALL_PATH'] = os.environ['SteamPath']
 os.environ['WINEPREFIX'] = "%s/pfx" % os.environ['STEAM_COMPAT_DATA_PATH']
+KNOWN_HASH_FILE = "know-hashes.json"
+KNOWN_HASHES = {}
 PALIA_ROOT = f"{os.environ['WINEPREFIX']}/drive_c/users/steamuser/AppData/Local/Palia"
+PALIA_INSTALLER_EXE = "PaliaInstaller.exe"
 PALIA_LAUNCHER_EXE = "PaliaLauncher.exe"
-PALIA_LAUNCHER = f"{PALIA_ROOT}/Launcher/{PALIA_LAUNCHER_EXE}"
+PALIA_LAUNCHER_ZIP = "PaliaLauncher-0.3.6.zip"
+PALIA_LAUNCHER_MANIFEST = "PaliaLauncher.json"
+PALIA_LAUNCHER_PATH = f"{PALIA_ROOT}/Launcher"
+PALIA_LAUNCHER = f"{PALIA_LAUNCHER_PATH}/{PALIA_LAUNCHER_EXE}"
 #For later
 #PALIA_LAUNCHER = f"{PALIA_ROOT}/Client/Palia/Binaries/Win64/PaliaClient-Win64-Shipping.exe"
-PALIA_LAUNCHER_URL = "https://update.palia.com/launcher/PaliaLauncher.exe"
+PALIA_LAUNCHER_HASH = "724640ab262eab5d52544717cdefa68122c2a688cc250a47cd8db595ec516349"
+PALIA_INSTALLER_URL = "https://update.palia.com/launcher/PaliaInstaller.exe"
+PALIA_LAUNCHER_URL = "https://update.palia.com/bundles/PaliaLauncher-0.3.6.zip"
+PALIA_LAUNCHER_MANIFEST_URL = "https://update.palia.com/manifest/PaliaLauncher.json"
 PALIA_MANIFEST_URL = "https://update.palia.com/manifest/PatchManifest.json"
 PALIA_MANIFEST = PALIA_MANIFEST_URL.rsplit('/', 1)[-1]
 PALIA_EULA_URL = "https://palia.com/terms"
@@ -142,18 +209,15 @@ if nr.returncode != 0:
 
 
 run(['curl', '-L', '-O', PALIA_MANIFEST_URL])
-mf = open(PALIA_MANIFEST, "r")
-mani = json.load(mf)
-mf.close()
+with open(PALIA_MANIFEST, "r") as mf:
+    mani = json.load(mf)
+
 basever = list(filter(lambda x: mani[x]['BaseLineVer'], mani))[0]
 baseverurl = mani[basever]['Files'][0]['URL']
 baseverzip = baseverurl.rsplit('/', 1)[-1]
 os.remove(PALIA_MANIFEST)
 
 rs = download_progress(VCREDIST_URL, VCREDIST, "Installing Palia", "Downloading VC Redist...")
-if rs != 0:
-    sys.exit(rs)
-rs = download_progress(PALIA_LAUNCHER_URL, VCREDIST, "Installing Palia", "Downloading Palia Launcher...")
 if rs != 0:
     sys.exit(rs)
 rs = download_progress(baseverurl, baseverzip, "Installing Palia", "Downloading base Palia version...")
@@ -208,7 +272,6 @@ run(['proton', 'run', f'{REGBATFILE}'])
 os.remove(REGBATFILE)
 os.remove(REGFILE)
 os.makedirs(f"{PALIA_ROOT}/Launcher/Downloads", exist_ok = True)
-os.renames(PALIA_LAUNCHER_EXE, PALIA_LAUNCHER)
 if zenity.poll() != None:
     sys.exit(zenity.returncode)
 zenity.stdin.write("# Installing VC Redist...\n")
