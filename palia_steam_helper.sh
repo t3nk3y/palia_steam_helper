cac576cc2a77064f54a5a80175df0ddce17c24ba
T3NK3Y <mattie@t3nk3y.com>
2023-10-03 13:47:14 -0400

    -Cant support direct launch options  since we are using palia launcher

--- palia_steam_helper.sh
+++ palia_steam_helper.sh
@@ -933,8 +933,6 @@ def get_base_zip():
 
 def launch_palia():
     launchopts = ["proton", "run", f"{PALIA_LAUNCHER}"]
-    if len(sys.argv) > 1:
-        launchopts = ["proton", "run", f"{PALIA_EXE}"] + sys.argv[1:]
     with Popen(launchopts, text=True, stdout=PIPE, stderr=STDOUT) as palia_proc:
         logging.debug(palia_proc.stdout.read())
     sys.exit(0)
